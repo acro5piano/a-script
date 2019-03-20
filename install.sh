87e8fbe096eeae7d461b9f1168594eaf6ee08b37
gosho-kazuya <ketsume0211@gmail.com>
2019-03-21 03:13:52 +0900

    can toggle

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl https://github.com/acro5piano/a-script/archive/v1.1.0.zip
--- main.js
+++ main.js
@@ -1,16 +1,57 @@
+function notify(text) {
+  const style = document.createElement('style')
+
+  const styled = {
+    div: (...literals) => literals[0],
+  }
+
+  const css = styled.div`
+    .notification {
+      position: fixed;
+      bottom: 64px;
+      left: 40%;
+      z-index: 1000;
+      width: 300px;
+      padding: 12px;
+      background: #333;
+      color: #fff;
+      border-radius: 3px;
+      opacity: 0.88;
+    }
+  `
+
+  style.type = 'text/css'
+  style.setAttribute('id', 'google-map-scraping-clipboard')
+  style.appendChild(document.createTextNode(css))
+
+  getHead().appendChild(style)
+
+  const div = document.createElement('div')
+  div.innerText = text
+  div.classList.add('notification')
+
+  getBody().appendChild(div)
+
+  setTimeout(() => div.remove(), 2000)
+}
+
+function getBody() {
+  return document.documentElement || document.body || document.querySelector('body')
+}
+
+function getHead() {
+  return document.documentElement || document.head || document.querySelector('head')
+}
+
 function copy(value) {
   const textarea = document.createElement('textarea')
 
   textarea.innerHTML = value
   textarea.setAttribute('id', 'content')
-
-  const body = document.documentElement || document.body || document.querySelector('body')
-  body.appendChild(textarea)
+  getBody().appendChild(textarea)
 
   textarea.select()
-
   document.execCommand('copy')
-
   textarea.remove()
 }
 
@@ -34,14 +75,28 @@ function formatRow(title, location) {
 
 function main() {
   const content = getContent()
-  console.log(content)
   copy(content)
+  notify('Content Copied!')
 }
 
-main()
+function getOnObject() {
+  const on = document.getElementById('on')
+  if (on) {
+    return on
+  }
+  const el = document.createElement('input')
+  el.setAttribute('id', 'on')
+  el.value = 'true'
+  getBody().appendChild(el)
+  return el
+}
 
-// chrome.tabs.getSelected(null, tab => {
-//   console.log('selected')
-// })
+function isOn() {
+  return getOnObject().value === 'true'
+}
 
-// setInterval(main, 2000)
+function toggle() {
+  getOnObject().value = getOnObject().value === 'true' ? 'false' : 'true'
+}
+
+setInterval(main, 3000)
